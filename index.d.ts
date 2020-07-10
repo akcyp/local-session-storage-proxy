166c00ef5a7266404535de37d9ee3899c2908855
akcyp <pycka.bartosz@gmail.com>
2020-07-10 13:31:52 +0200

    Types fix

--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,2 @@
-declare module "createProxyStorage" {
-  export default function createProxyStorage<T = {}>(storage_id: string, keys: T = {}, root = window.localStorage, strict = false): T;
-}
+function createProxyStorage<T = {}>(storage_id: string, keys: T = {}, root: any = window.localStorage, strict = false): T;
+export = createProxyStorage;
